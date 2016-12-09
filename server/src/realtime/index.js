d59b62d3d9f3013ae3c16a369729a835216109a3
Javier Pérez <surtich@gmail.com>
2016-12-09 23:58:16 +0100

    [client] Websocket conn for realtime is now created when login and closed when logout

--- server/src/realtime/index.js
+++ server/src/realtime/index.js
@@ -1,18 +1,50 @@
-import {listen} from 'rethinkdb-websocket-server';
+import {listen, r, RP} from 'rethinkdb-websocket-server';
+import {verify} from 'jsonwebtoken';
 
-import {db as dbConfig} from '../../config';
+import {db as dbConfig, auth as authConfig} from '../../config';
 
 const options = {
   httpPath: '/realtime',
-  dbHost: dbConfig.host,
-  dbPort: dbConfig.port,
+  host: dbConfig.host,
+  port: dbConfig.port,
   db: dbConfig.db,
   unsafelyAllowAnyQuery: true,
 };
 
+const rethinkConn = r.connect(options);
+
+const runQuery = query => rethinkConn.then(
+  conn => query.run(conn)
+);
+
+
+const sessionCreator = urlQueryParams => {
+  const {authToken} = urlQueryParams;
+  if (!authToken) {
+    return Promise.reject('Missing auth token');
+  }
+  try {
+    const user = verify(authToken, authConfig.jwtSecret);
+    const userQuery = r.table('User').get(user.id);
+    return runQuery(userQuery).then(userDB => ({user: userDB}));
+  } catch (err) {
+    return Promise.reject('Invalid auth token');
+  }
+};
+
+const queryWhitelist = [
+  r.table('Question')
+  .filter({id: RP.ref('id')})
+  .changes()
+  .opt('db', r.db('expertsdb'))
+  .validate(() => true),
+];
+
 export default (httpServer) => {
   listen({
     ...options,
+    sessionCreator,
+    queryWhitelist,
     httpServer,
   });
 };
